3de3b146cf419d2dbd4aec35af7dd096a8170fd7
Anna Veselova <annakartashevamail@gmail.com>
2025-06-13 22:52:29 +0300

    minor fixes

--- task1/solution.py
+++ task1/solution.py
@@ -4,26 +4,22 @@ from typing import Callable
 def strict(
     function: Callable[..., bool | int | float | str],
 ) -> Callable[..., bool | int | float | str]:
-    """Throws TypeError if wrapped function arguments are not as annotated"""
+    """Checks wrapped function arguments against their type hints"""
 
     def wrapper(*args, **kwargs):
-        # fetch arguments and their types
-        arguments_with_types: dict = function.__annotations__
-        arguments_with_types.pop("return", None)
+        arguments_with_types = function.__annotations__
+        arguments_with_types.pop(
+            "return", None
+        )  # dunder also exposes return of function; remove it
 
-        # it seems that this decorator is designed to demand strict typing;
-        # that is why it will raise TypeError if any of arguments is not annotated
+        # throws error if any argument is not annotated
         if len(arguments_with_types) < (len(args) + len(kwargs)):
             raise TypeError
 
-        # function may have a mix of args and kwargs; process all kwargs first
         for kwarg in kwargs:
             if not isinstance(kwargs[kwarg], arguments_with_types[kwarg]):
                 raise TypeError
             arguments_with_types.pop(kwarg, None)
-
-        # now only args are left in the arguments_with_types;
-        # order in *args and in arguments_with_types is the same as since python 3.7 dicts are ordered
 
         for arg, arg_type in zip(args, arguments_with_types.values()):
             if not isinstance(arg, arg_type):
